4e134aa0f963ee0060210c4dcdb571ccfd6c29f2
Fahri <111134259+fahrikurniawan99@users.noreply.github.com>
2023-12-02 06:27:09 +0700

    feat: create jwt service, implement in user handler

--- handler/user.go
+++ handler/user.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"bwastartup/auth"
 	"bwastartup/helper"
 	"bwastartup/user"
 	"fmt"
@@ -11,10 +12,11 @@ import (
 
 type userHandler struct {
 	userService user.Service
+	authService auth.Service
 }
 
-func NewUserHandler(userService user.Service) *userHandler {
-	return &userHandler{userService}
+func NewUserHandler(userService user.Service, authService auth.Service) *userHandler {
+	return &userHandler{userService, authService}
 }
 
 func (h *userHandler) RegisterUser(c *gin.Context) {
@@ -40,7 +42,15 @@ func (h *userHandler) RegisterUser(c *gin.Context) {
 		return
 	}
 
-	formatter := user.FormatUser(newUser, "")
+	token, err := h.authService.GenerateToken(newUser.ID)
+
+	if err != nil {
+		response := helper.APIResponse("Register user failed", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	formatter := user.FormatUser(newUser, token)
 
 	response := helper.APIResponse("Accout has ben register", http.StatusOK, "success", formatter)
 
@@ -74,7 +84,15 @@ func (h *userHandler) LoginUser(c *gin.Context) {
 			return
 		}
 
-		formatter := user.FormatUser(loggedUser, "")
+		token, err := h.authService.GenerateToken(loggedUser.ID)
+
+	if err != nil {
+		response := helper.APIResponse("Login failed", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+		formatter := user.FormatUser(loggedUser, token)
 
 		response := helper.APIResponse("Successfuly loggedin", http.StatusOK, "success", formatter)
 
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bwastartup/auth"
 	"bwastartup/handler"
 	"bwastartup/user"
 	"fmt"
@@ -22,9 +23,12 @@ func main() {
 	fmt.Println("Connection to database is good")
 
 	userRepository := user.NewRepository(db)
-	userService := user.NewService(userRepository)
-	userHandler := handler.NewUserHandler(userService)
 
+	userService := user.NewService(userRepository)
+	authService := auth.NewService()
+	
+	userHandler := handler.NewUserHandler(userService, authService)
+	
 	router := gin.Default()
 
 	api := router.Group("/api/v1")
